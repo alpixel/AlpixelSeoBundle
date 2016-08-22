d2e7f1eacb994c73f02a51a3081f65f8e99992a0
Benjamin HUBERT <benjamin@alpixel.fr>
2016-08-22 17:04:59 +0200

    Added a helper to generate alternative links

---
+++ Helper/SEOHelper.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Alpixel\Bundle\SEOBundle\Helper;
+
+use Sonata\SeoBundle\Seo\SeoPageInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
+
+
+/**
+ * @author Benjamin HUBERT <benjamin@alpixel.fr>
+ */
+class SEOHelper
+{
+
+    private $requestStack;
+    private $enabledLocales;
+    private $seoHelper;
+    private $router;
+
+    public function __construct(
+        RequestStack $requestStack,
+        $enabledLocales,
+        SeoPageInterface $seoHelper,
+        RouterInterface $router
+    ) {
+        $this->requestStack = $requestStack;
+        $this->enabledLocales = $enabledLocales;
+        $this->seoHelper = $seoHelper;
+        $this->router = $router;
+    }
+
+    public function generateAlternateLangLinks($extraParams = array())
+    {
+        $request = $this->requestStack->getMasterRequest();
+        $route = $request->attributes->get('_route');
+
+        foreach ($this->enabledLocales as $locale) {
+            if ($request->getLocale() !== $locale) {
+                $this->seoHelper->addLangAlternate(
+                    $this->router->generate(
+                        $route,
+                        array_merge(
+                            $request->attributes->get('_route_params'),
+                            [
+                                '_locale' => $locale,
+                            ],
+                            $extraParams
+                        ),
+                        Router::ABSOLUTE_URL
+                    ),
+                    $locale
+                );
+            }
+        }
+    }
+
+
+}
